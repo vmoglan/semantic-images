9947175f19ff4780a035d45b12374b0d552da382
Vlad-Adrian Moglan <vlad-adrian.moglan@students.hs-mainz.de>
2019-01-03 17:43:38 +0100

    Added image resize methods.

---
+++ SemanticImages.Tests/ResizerTests.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Drawing;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SemanticImages.Tests.ImageProcessing
+{
+    [TestClass]
+    public class ResizerTests
+    {
+        private const int INITIAL_WIDTH = 1024;
+        private const int INITIAL_HEIGHT = 768;
+
+        [TestMethod]
+        public void ShouldResizeImage()
+        {
+            const int NEW_WIDTH = 100;
+            const int NEW_HEIGHT = 100;
+
+            Image sourceImage = new Bitmap(INITIAL_WIDTH, INITIAL_HEIGHT);
+            Image destImage = ImageUtils.Resize(sourceImage, NEW_WIDTH, NEW_HEIGHT);
+
+            Assert.AreEqual(destImage.Width, NEW_WIDTH);
+            Assert.AreEqual(destImage.Height, NEW_HEIGHT);
+        }
+
+        [TestMethod]
+        public void ShouldResizeImageMaintainingAspectRation()
+        {
+            const float SCALE = 1.75f;
+
+            Image sourceImage = new Bitmap(INITIAL_WIDTH, INITIAL_HEIGHT);
+            Image destImage = ImageUtils.Resize(sourceImage, SCALE);
+
+            Assert.AreEqual(destImage.Width / destImage.Height, sourceImage.Width / sourceImage.Height);
+        }
+    }
+}
---
+++ SemanticImages/ImageUtils.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Drawing.Imaging;
+
+namespace SemanticImages
+{
+    /// <summary>
+    /// Used for resizing an image.
+    /// </summary>
+    public static class ImageUtils
+    {
+        /// <summary>
+        /// Resizes an image without necessarily keeping the aspect ratio.
+        /// </summary>
+        /// <param name="image">the image to be resized</param>
+        /// <param name="width">new image width</param>
+        /// <param name="height">new image height</param>
+        public static Bitmap Resize(Image image, int width, int height)
+        {
+            if (width <= 0 || height <= 0)
+            {
+                throw new ArgumentException("Width and height must be strictly positive.");
+            }
+
+            Rectangle r = new Rectangle(0, 0, width, height);
+            Bitmap result = new Bitmap(width, height);
+
+            result.SetResolution(image.HorizontalResolution, image.VerticalResolution);
+
+            using (var graphics = Graphics.FromImage(result))
+            {
+                graphics.CompositingMode = CompositingMode.SourceCopy;
+                graphics.CompositingQuality = CompositingQuality.HighQuality;
+                graphics.InterpolationMode = InterpolationMode.HighQualityBicubic;
+                graphics.SmoothingMode = SmoothingMode.HighQuality;
+                graphics.PixelOffsetMode = PixelOffsetMode.HighQuality;
+
+                using (var wrapMode = new ImageAttributes())
+                {
+                    wrapMode.SetWrapMode(WrapMode.TileFlipXY);
+                    graphics.DrawImage(image, r, 0, 0, image.Width, image.Height, GraphicsUnit.Pixel);
+                }
+            }
+
+            return result;
+        }
+
+        /// <summary>
+        /// Resizes an image keeping the aspect ratio.
+        /// </summary>
+        /// <param name="image">the image to be resized</param>
+        /// <param name="scale">the factor by which the image is to be scaled</param>
+        public static Bitmap Resize(Image image, float scale)
+        {
+            if (scale <= 0)
+            {
+                throw new ArgumentException("Scale must be strictly positive.");
+            }
+
+            return Resize(image, (int) (Math.Floor(image.Width * scale)), (int) (Math.Floor(image.Height * scale)));
+        }
+    }
+}
