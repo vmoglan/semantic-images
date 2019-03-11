0e8acecb31ddaa6aed970b5585e8139052356437
Moglan <C9935083@eaton.ad.etn.com>
2019-03-11 14:54:11 +0100

    Added ImageModificationManager and tests.

---
+++ SemanticImages.Core/ImageModificationManager.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.IO;
+
+namespace SemanticImages.Core
+{
+    public class ImageModificationManager :
+        INotifyPropertyChanged
+    {
+        private Stack<Bitmap> _history;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public Bitmap LastModification
+        {
+            get => _history.Peek();
+            private set { }
+        }
+
+        public ImageModificationManager(Bitmap sourceImage)
+        {
+            _history = new Stack<Bitmap>();
+
+            _history.Push(sourceImage);
+        }
+
+        public void Resize(int width, int height)
+        {
+            _history.Push(ImageUtils.Resize(LastModification, width, height));
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(LastModification)));
+        }
+
+        public void Resize(float scale)
+        {
+            _history.Push(ImageUtils.Resize(LastModification, scale));
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(LastModification)));
+        }
+
+        public void Crop(Rectangle r)
+        {
+            _history.Push(ImageUtils.Crop(LastModification, r));
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(LastModification)));
+        }
+
+        public void Undo()
+        {
+            _history.Pop();
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(LastModification)));
+        }
+
+        public void UndoAll()
+        {
+            while (_history.Count > 1)
+            {
+                _history.Pop();
+            }
+
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(LastModification)));
+        }
+    }
+}
